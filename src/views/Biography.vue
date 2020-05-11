78c09102e41bbb430fa9bb22a1b852176667d951
Dominik.L <36797134+dominiklippl@users.noreply.github.com>
2020-05-11 11:23:08 +0200

    Updated age.

--- src/views/Biography.vue
+++ src/views/Biography.vue
@@ -3,7 +3,7 @@
         <h1 class="mb-4">Biografie</h1>
 
         <b-card-text class="text-left">
-            Ich bin 17 Jahre alt, und wohne in dem wünderschönen Niederbayern.
+            Ich bin 18 Jahre alt, und wohne in dem wünderschönen Niederbayern.
             Momentan mache ich eine Ausbildung zum Fachinformatiker für Anwendungsentwicklung.
             In meiner Freizeit arbeite ich als Administrator und Entwickler bei diesem <a href="http://mcone.eu" target="_blank" style="font-weight: bold">Projekt.</a>
             Mein Schwerpunkt liegt auf der Anwedungsentwicklung mit Java & C#.
